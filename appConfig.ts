a4b06e2e2290dbb5652d18023d1f70296ce08155
gregogun <gregogun97@gmail.com>
2023-10-14 08:51:40 +0100

    feat: add app config

---
+++ appConfig.ts
@@ -0,0 +1,8 @@
+export const appConfig = {
+  defaultGateway: "https://arweave.net",
+  accountBannerDefault:
+    "https://arweave.net:443/a0ieiziq2JkYhWamlrUCHxrGYnHWUAMcONxRmfkWt-k",
+  accountAvatarDefault:
+    "https://arweave.net:443/OrG-ZG2WN3wdcwvpjz1ihPe4MI24QBJUpsJGIdL85wA",
+  playerMaxHeight: 76,
+};
