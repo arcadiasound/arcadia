444b609a9caa33f41d9db527fd05d0702d4f10e5
gregogun <gregogun97@gmail.com>
2023-12-01 12:53:43 +0000

    feat: redesign profile

--- modules/profile/index.tsx
+++ modules/profile/index.tsx
@@ -13,9 +13,46 @@ import { TrackCard } from "../track/TrackCard";
 import { styled } from "@/stitches.config";
 import { useState } from "react";
 import { useConnect } from "@/hooks/useConnect";
+import { Skeleton } from "@/ui/Skeleton";
+
+const TrackSkeleton = styled(Skeleton, {
+  width: 250,
+  height: 250,
+});
+
+const StyledTabsTrigger = styled(TabsTrigger, {
+  br: "$1",
+
+  "&:hover": {
+    backgroundColor: "$slate2",
+    color: "$slate12",
+  },
+
+  '&[data-state="active"]': {
+    backgroundColor: "$slate3",
+    color: "$slate12",
+    boxShadow: "none",
+    fontWeight: 400,
+  },
+
+  variants: {
+    size: {
+      1: {
+        fontSize: "$2",
+        px: "$5",
+        py: "$2",
+      },
+      2: {
+        fontSize: "$3",
+        px: "$5",
+        py: "$2",
+      },
+    },
+  },
+});
 
 const StyledTabsContent = styled(TabsContent, {
-  pt: "$5",
+  pt: "$10",
 });
 
 type TrackTab = "tracks" | "likes";
@@ -44,14 +81,14 @@ export const Profile = () => {
     },
   });
 
-  const { data: tracks } = useQuery({
+  const { data: tracks, isLoading: tracksLoading } = useQuery({
     queryKey: [`tracks-by=${addr}`],
     queryFn: () => {
       if (!addr) {
         if (walletAddress) {
           return getTrackByOwners(walletAddress);
         } else {
-          throw new Error("No profile has been found");
+          return;
         }
       }
 
@@ -71,11 +108,11 @@ export const Profile = () => {
   // }
 
   return (
-    <Flex direction="column" gap="5">
+    <Flex direction="column">
       <Box
         css={{
           width: "100%",
-          height: 220,
+          height: 280,
           aspectRatio: 16 / 9,
           backgroundSize: "cover",
           backgroundPosition: "center",
@@ -88,60 +125,98 @@ export const Profile = () => {
             height: 280,
           },
         }}
-      ></Box>
-      <Flex gap="5" align="center">
-        <Image
-          css={{
-            width: 150,
-            height: 150,
-            br: 9999,
-          }}
-          src={
-            avatarUrl === appConfig.accountAvatarDefault
-              ? `https://source.boringavatars.com/marble/100/${account?.txid}?square=true`
-              : avatarUrl
-          }
-        />
-        <Flex direction="column" gap="3">
-          <Box>
-            <Typography size="5" contrast="hi">
-              {account?.profile.name ||
-                abbreviateAddress({
-                  address: account?.addr,
-                  options: { startChars: 6, endChars: 6 },
-                })}
-            </Typography>
-            {account && account.profile.handleName && (
-              <Typography size="2">@{account.profile.handleName}</Typography>
-            )}
-          </Box>
-          <Typography>{account?.profile.bio || "No bio."}</Typography>
-        </Flex>
-      </Flex>
-      <Tabs
-        onValueChange={(e) => setActiveTab(e as TrackTab)}
-        defaultValue="tracks"
+      />
+      <Box
+        css={{
+          display: "grid",
+          gridTemplateColumns: "280px 1fr",
+          // maxWidth: 1400,
+          gap: 120,
+        }}
       >
-        <TabsList>
-          <TabsTrigger value="tracks">tracks</TabsTrigger>
-          {/* <TabsTrigger value="albums">albums</TabsTrigger> */}
-          {/* <TabsTrigger value="likes">likes</TabsTrigger> */}
-        </TabsList>
-        <StyledTabsContent value="tracks">
-          {tracks && tracks.length > 0 && (
-            <Flex wrap="wrap" gap="10">
-              {tracks.map((track, idx) => (
-                <TrackCard
-                  key={track.txid}
-                  track={track}
-                  trackIndex={idx}
-                  tracks={tracks}
-                />
-              ))}
+        <Flex
+          css={{
+            mt: -60,
+            alignSelf: "start",
+            ml: 80,
+          }}
+          direction="column"
+          gap="5"
+          // align="center"
+        >
+          <Image
+            css={{
+              width: 120,
+              height: 120,
+              br: 9999,
+              outline: "4px solid $colors$whiteA5",
+              outlineOffset: -4,
+            }}
+            src={
+              avatarUrl === appConfig.accountAvatarDefault
+                ? `https://source.boringavatars.com/marble/100/${account?.txid}?square=true`
+                : avatarUrl
+            }
+          />
+          <Flex direction="column" gap="3">
+            <Flex direction="column" gap="1">
+              <Typography size="6" weight="5" contrast="hi">
+                {account?.profile.name ||
+                  abbreviateAddress({
+                    address: account?.addr,
+                    options: { startChars: 6, endChars: 6 },
+                  })}
+              </Typography>
+              {account && account.profile.handleName && (
+                <Typography size="1">@{account.profile.handleName}</Typography>
+              )}
             </Flex>
-          )}
-        </StyledTabsContent>
-        {/* <StyledTabsContent value="tracks">
+            <Typography>{account?.profile.bio || "No bio."}</Typography>
+          </Flex>
+        </Flex>
+        <Tabs
+          css={{ mt: 80 }}
+          onValueChange={(e) => setActiveTab(e as TrackTab)}
+          defaultValue="tracks"
+        >
+          <TabsList
+            css={{
+              br: "$2",
+              width: "max-content",
+              gap: "$2",
+              backgroundColor: "$slate1",
+              boxShadow: "0 0 0 1px $colors$slate5",
+              p: "$1",
+            }}
+          >
+            <StyledTabsTrigger value="tracks">releases</StyledTabsTrigger>
+            <StyledTabsTrigger value="likes">likes</StyledTabsTrigger>
+          </TabsList>
+          <StyledTabsContent value="tracks">
+            {tracksLoading && (
+              <Flex wrap="wrap" gap="10">
+                <TrackSkeleton />
+                <TrackSkeleton />
+                <TrackSkeleton />
+                <TrackSkeleton />
+                <TrackSkeleton />
+              </Flex>
+            )}
+            {tracks && tracks.length > 0 && (
+              <Flex wrap="wrap" gap="10">
+                {tracks.map((track, idx) => (
+                  <TrackCard
+                    size={250}
+                    key={track.txid}
+                    track={track}
+                    trackIndex={idx}
+                    tracks={tracks}
+                  />
+                ))}
+              </Flex>
+            )}
+          </StyledTabsContent>
+          {/* <StyledTabsContent value="tracks">
           {likedTracks && likedTracks.length > 0 && (
             <Flex wrap="wrap" gap="10">
               {likedTracks.map((track, idx) => (
@@ -155,7 +230,8 @@ export const Profile = () => {
             </Flex>
           )}
         </StyledTabsContent> */}
-      </Tabs>
+        </Tabs>
+      </Box>
     </Flex>
   );
 };
