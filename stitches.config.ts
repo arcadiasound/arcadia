e25552e5bc3f5b3562ca035ed0ac4578d319cc31
gregogun <gregogun97@gmail.com>
2023-12-04 16:35:55 +0000

    feat: ui enhancements

--- stitches.config.ts
+++ stitches.config.ts
@@ -107,6 +107,15 @@ export const {
       toast: 1700,
       tooltip: 1800,
     },
+    shadows: {
+      1: "0px 1px 2px rgba(0, 0, 0, 0.05)",
+      2: "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+      3: "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+      4: "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)",
+      5: "0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)",
+      6: "0 25px 50px -12px rgb(0 0 0 / 0.25)",
+      inner: "box-shadow: inset 0 2px 4px 0 rgb(0 0 0 / 0.05)",
+    },
   },
   media: {
     bp1: "(min-width: 520px)",
