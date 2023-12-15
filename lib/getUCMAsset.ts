e3f99bf87752eb814ff0f04d0cae88e154385d6e
gregogun <gregogun97@gmail.com>
2023-12-15 21:26:15 +0000

    feat: show track owners

---
+++ lib/getUCMAsset.ts
@@ -0,0 +1,22 @@
+import { appConfig } from "@/appConfig";
+import { UCMAssetProps } from "@/types";
+
+export const getUCMAsset = async (assetId: string) => {
+  try {
+    const res = await fetch(
+      `${appConfig.dreU}/contract?id=${assetId}&errorMessages=true`
+    );
+
+    if (!res.ok) {
+      throw new Error(
+        `Error occurred getting asset listed status: ${res.statusText} with code ${res.status}`
+      );
+    }
+
+    const data: UCMAssetProps = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
