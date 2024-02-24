18c270cb9f4225ea98fde4df2f0aa7719b413739
gregogun <gregogun97@gmail.com>
2024-02-24 17:01:07 +0000

    feat: update profile page

--- apps/arcadia/src/modules/profile/index.tsx
+++ apps/arcadia/src/modules/profile/index.tsx
@@ -1,316 +1,217 @@
-import { Box } from "@/ui/Box";
-import { Flex } from "@/ui/Flex";
-import { Image } from "@/ui/Image";
-import { Typography } from "@/ui/Typography";
-import { useLocation } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import { getProfile } from "@/lib/getProfile";
+import { appConfig } from "@/config";
+import { useGetUserProfile } from "@/hooks/appData";
+import { css } from "@/styles/css";
 import { abbreviateAddress } from "@/utils";
-import { appConfig } from "@/apps/arcadia/appConfig";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui/Tabs";
-import { getTrackByOwners } from "@/lib/getTrackByOwner";
-import { TrackCard } from "../track/TrackCard";
-import { styled } from "@/apps/arcadia/stitches.config";
-import { useEffect, useState } from "react";
-import { useConnect } from "@/hooks/useConnect";
-import { Skeleton } from "@/ui/Skeleton";
-import { getLikedTracks } from "@/lib/getLikedTracks";
-import { getAssetCollection } from "@/lib/getAssetCollection";
-import { Avatar, AvatarFallback, AvatarImage } from "@/ui/Avatar";
+import {
+  AspectRatio,
+  Avatar,
+  Box,
+  Flex,
+  Grid,
+  IconButton,
+  Separator,
+  Text,
+} from "@radix-ui/themes";
+import { styled } from "@stitches/react";
+import { useActiveAddress } from "arweave-wallet-kit";
+import BoringAvatar from "boring-avatars";
+import { BsCopy, BsPatchCheckFill } from "react-icons/bs";
 
-const TrackSkeleton = styled(Skeleton, {
-  width: 200,
-  height: 200,
-});
+const StyledBoringAvatar = styled(BoringAvatar);
 
-const StyledTabsTrigger = styled(TabsTrigger, {
-  br: "$1",
+const BANNER_RADIUS = `max(var(--radius-1), var(--radius-4) * 0.8)`;
+const AVATAR_RADIUS = `max(var(--radius-3), var(--radius-full) * 0.8)`;
+const AVATAR_SIZE = 120;
+const OUTLINE_OFFSET = 2;
+const BANNER_HEIGHT = 280;
+const VOUCHED_ICON_SIZE = 12;
 
-  "&:hover": {
-    backgroundColor: "$slate3",
-    color: "$slate12",
+const AlphaIconButton = styled(IconButton, {
+  color: "var(--white-a8)",
+
+  "& svg": {
+    width: 12,
+    height: 12,
   },
 
-  '&[data-state="active"]': {
-    backgroundColor: "$slate4",
-    color: "$slate12",
-    boxShadow: "none",
-    fontWeight: 400,
+  "&:hover": {
+    // backgroundColor: "var(--white-a4)",
+    color: "var(--white-a12)",
   },
 
   variants: {
-    size: {
-      1: {
-        fontSize: "$2",
-        px: "$5",
-        py: "$2",
-      },
-      2: {
-        fontSize: "$3",
-        px: "$5",
-        py: "$2",
+    liked: {
+      true: {
+        color: "var(--accent-9)",
+        "&:hover": {
+          backgroundColor: "var(--white-a4)",
+          color: "var(--accent-10)",
+        },
       },
     },
   },
 });
 
-const StyledTabsContent = styled(TabsContent, {
-  pt: "$10",
-});
-
-type TrackTab = "tracks" | "likes" | "collection";
-
 export const Profile = () => {
-  const location = useLocation();
-  const query = location.search;
+  const address = useActiveAddress();
+  const query = typeof window !== "undefined" ? window.location.search : "";
   const urlParams = new URLSearchParams(query);
-  const { walletAddress } = useConnect();
-  const [activeTab, setActiveTab] = useState<TrackTab>("tracks");
+  const addressFromParams = urlParams.get("addr");
 
-  const addr = urlParams.get("addr");
+  const addr = addressFromParams || address;
 
-  const address = addr || walletAddress;
+  const { data } = useGetUserProfile({ address: addr });
+  const profile = data?.length ? data[0] : undefined;
 
-  const { data: account, isError } = useQuery({
-    queryKey: [`profile-${address}`],
-    queryFn: () => {
-      if (address) {
-        return getProfile(address);
-      } else {
-        return;
-      }
-    },
-  });
+  const bannerUrl = profile?.bannerSrc
+    ? profile.bannerSrc
+    : `${appConfig.boringAvatarsUrl}/marble/${AVATAR_SIZE}/${addr}?square=true`;
 
-  const { data: tracks, isLoading: tracksLoading } = useQuery({
-    queryKey: [`tracks-by=${address}`],
-    refetchOnWindowFocus: false,
-    enabled: activeTab === "tracks" && !!address,
-    queryFn: () => {
-      if (address) {
-        return getTrackByOwners(address);
-      } else {
-        return;
-      }
-    },
-  });
+  const avatarUrl = profile?.avatarSrc
+    ? profile.avatarSrc
+    : `${appConfig.boringAvatarsUrl}/marble/${AVATAR_SIZE}/${addr}?square=true`;
 
-  const { data: likedTracks, isLoading: likedTracksLoading } = useQuery({
-    queryKey: [`liked-tracks-${address}`],
-    refetchOnWindowFocus: false,
-    enabled: activeTab === "likes" && !!address,
-    queryFn: () => {
-      if (address) {
-        return getLikedTracks(address);
-      } else {
-        return;
-      }
-    },
-  });
-
-  const { data: assetCollection, isLoading: assetCollectionLoading } = useQuery(
-    {
-      queryKey: [`assetCollectio0-${address}`],
-      refetchOnWindowFocus: false,
-      enabled: activeTab === "collection" && !!address,
-      queryFn: () => {
-        if (address) {
-          return getAssetCollection(address);
-        } else {
-          return;
-        }
-      },
-    }
-  );
-
-  const bannerUrl = account?.profile.bannerURL;
-  const avatarUrl = account?.profile.avatarURL;
-
-  useEffect(() => {
-    if (typeof window !== "undefined" && window.scrollY) {
-      window.scroll(0, 0);
-    }
-  }, []);
+  if (!addr) {
+    // temp
+    return (
+      <Grid>
+        <Text>No wallet found</Text>
+      </Grid>
+    );
+  }
 
   return (
-    <Flex direction="column">
-      <Box
-        css={{
+    <Box
+      style={css({
+        width: "100%",
+        height: BANNER_HEIGHT,
+        position: "relative",
+      })}
+    >
+      <Avatar
+        src={bannerUrl}
+        fallback={<StyledBoringAvatar name={addr} variant="marble" />}
+        style={css({
           width: "100%",
-          height: 300,
-          aspectRatio: 16 / 9,
-        }}
-      >
-        {address ? (
-          <Box
-            css={{
-              width: "100%",
-              height: "100%",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundImage: `url(${
-                bannerUrl === appConfig.accountBannerDefault
-                  ? `https://source.boringavatars.com/marble/1000/${account?.txid}?square=true`
-                  : bannerUrl
-              })`,
-            }}
-          />
-        ) : (
-          <Skeleton
-            css={{
-              width: "100%",
-              height: "100%",
-            }}
-          />
-        )}
-      </Box>
+          height: "100%",
+          aspectRatio: 3 / 1,
+          borderRadius: 0,
+        })}
+      />
       <Box
-        css={{
-          display: "grid",
-          gridTemplateColumns: "300px 1fr",
-          gap: 120,
-        }}
+        style={css({
+          position: "absolute",
+          inset: 0,
+          background: `linear-gradient(
+              to top,
+              var(--black-a9) 0%,
+              var(--black-a3) 50%,
+              var(--black-a1) 65%,
+              var(--black-a1) 75.5%,
+              var(--black-a1) 82.85%,
+              var(--black-a1) 88%,
+              var(--black-a1) 100%
+                )`,
+        })}
+      />
+
+      <Flex
+        gap="3"
+        mx="4"
+        pb="4"
+        align="end"
+        style={css({
+          position: "absolute",
+          inset: 0,
+        })}
       >
+        <Avatar
+          src={avatarUrl}
+          fallback={<BoringAvatar size={AVATAR_RADIUS} name={addr} variant="marble" />}
+          style={css({
+            width: AVATAR_SIZE,
+            height: AVATAR_SIZE,
+            borderRadius: AVATAR_RADIUS,
+            outline: `${OUTLINE_OFFSET}px solid var(--white-a3)`,
+            outlineOffset: -OUTLINE_OFFSET,
+          })}
+        />
         <Flex
-          css={{
-            mt: -60,
-            alignSelf: "start",
-            ml: 80,
-          }}
           direction="column"
-          gap="5"
+          style={css({
+            color: "var(--white-a12)",
+          })}
         >
-          <Avatar size="8">
-            <AvatarImage
-              css={{
-                width: 120,
-                height: 120,
-                br: 9999,
-                outline: "4px solid $colors$whiteA5",
-                outlineOffset: -4,
-              }}
-              src={
-                avatarUrl === appConfig.accountAvatarDefault
-                  ? `https://source.boringavatars.com/marble/100/${account?.txid}?square=true`
-                  : avatarUrl
-              }
+          <Flex align="center" gap="1">
+            <Text size="1" weight="medium">
+              Verified
+            </Text>
+            <BsPatchCheckFill
+              style={css({
+                width: VOUCHED_ICON_SIZE,
+                height: VOUCHED_ICON_SIZE,
+              })}
             />
-            <AvatarFallback
-              css={{
-                width: 120,
-                height: 120,
-                br: 9999,
-                outline: "4px solid $colors$whiteA5",
-                outlineOffset: -4,
-                backgroundColor: "$slate2",
-              }}
-            >
-              nJ
-            </AvatarFallback>
-          </Avatar>
-          <Flex direction="column" gap="3">
-            <Flex direction="column" gap="1">
-              <Typography size="6" weight="5" contrast="hi">
-                {account?.profile.name ||
-                  abbreviateAddress({
-                    address: account?.addr,
-                    options: { startChars: 6, endChars: 6 },
-                  })}
-              </Typography>
-              {account && account.profile.handleName && (
-                <Typography size="1">@{account.profile.handleName}</Typography>
-              )}
-            </Flex>
-            <Typography>{account?.profile.bio || "No bio."}</Typography>
           </Flex>
-        </Flex>
-        <Tabs
-          css={{ mt: 40 }}
-          onValueChange={(e) => setActiveTab(e as TrackTab)}
-          defaultValue="tracks"
-        >
-          <TabsList
-            css={{
-              br: "$2",
-              width: "max-content",
-              gap: "$1",
-              backgroundColor: "$slate1",
-              boxShadow: "0 0 0 1px $colors$slate5",
-              p: "$1",
-            }}
+          <Flex
+            align="center"
+            gap="3"
+            style={css({
+              backdropFilter: "blur(4px)",
+              borderRadius: BANNER_RADIUS,
+            })}
           >
-            <StyledTabsTrigger value="tracks">releases</StyledTabsTrigger>
-            <StyledTabsTrigger value="likes">likes</StyledTabsTrigger>
-            <StyledTabsTrigger value="collection">collection</StyledTabsTrigger>
-          </TabsList>
-          <StyledTabsContent value="tracks">
-            {tracksLoading && (
-              <Flex wrap="wrap" gap="10">
-                <TrackSkeleton />
-                <TrackSkeleton />
-                <TrackSkeleton />
-                <TrackSkeleton />
-              </Flex>
-            )}
-            {tracks && tracks.length > 0 && (
-              <Flex wrap="wrap" gap="10">
-                {tracks.map((track, idx) => (
-                  <TrackCard
-                    key={track.txid}
-                    track={track}
-                    trackIndex={idx}
-                    tracks={tracks}
-                  />
-                ))}
-              </Flex>
-            )}
-          </StyledTabsContent>
-          <StyledTabsContent value="likes">
-            {likedTracksLoading && (
-              <Flex wrap="wrap" gap="10">
-                <TrackSkeleton />
-                <TrackSkeleton />
-                <TrackSkeleton />
-                <TrackSkeleton />
-              </Flex>
-            )}
-            {likedTracks && likedTracks.length > 0 && (
-              <Flex wrap="wrap" gap="10">
-                {likedTracks.map((track, idx) => (
-                  <TrackCard
-                    key={track.txid}
-                    track={track}
-                    trackIndex={idx}
-                    tracks={likedTracks}
-                  />
-                ))}
-              </Flex>
-            )}
-          </StyledTabsContent>
-          <StyledTabsContent value="collection">
-            {assetCollectionLoading && (
-              <Flex wrap="wrap" gap="10">
-                <TrackSkeleton />
-                <TrackSkeleton />
-                <TrackSkeleton />
-                <TrackSkeleton />
-              </Flex>
-            )}
-            {assetCollection && assetCollection.length > 0 && (
-              <Flex wrap="wrap" gap="10">
-                {assetCollection.map((track, idx) => (
-                  <TrackCard
-                    key={track.txid}
-                    track={track}
-                    trackIndex={idx}
-                    tracks={assetCollection}
-                  />
-                ))}
-              </Flex>
-            )}
-          </StyledTabsContent>
-        </Tabs>
-      </Box>
-    </Flex>
+            <Text
+              size="9"
+              weight="medium"
+              style={css({
+                textOverflow: "ellipsis",
+                whiteSpace: "nowrap",
+                overflow: "hidden",
+                maxWidth: "15ch",
+                // solves issue of overflow cutting off text
+                lineHeight: 1.15,
+              })}
+            >
+              {profile?.name || abbreviateAddress({ address: addr })}
+            </Text>
+          </Flex>
+          {profile?.name && (
+            <Flex
+              align="center"
+              gap="2"
+              mt="1"
+              pl="1"
+              style={css({
+                color: "var(--white-a10)",
+              })}
+            >
+              {profile?.handle && (
+                <>
+                  <Text size="2">
+                    {profile.handle.startsWith("@") ? profile.handle : `@${profile.handle}`}
+                  </Text>
+                  <Separator orientation="vertical" />
+                </>
+              )}
+              <Text
+                size="2"
+                style={css({
+                  textOverflow: "ellipsis",
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  maxWidth: "20ch",
+                })}
+              >
+                {abbreviateAddress({ address: addr })}
+              </Text>
+              <AlphaIconButton size="1" color="gray" variant="ghost">
+                <BsCopy />
+              </AlphaIconButton>
+            </Flex>
+          )}
+        </Flex>
+      </Flex>
+    </Box>
   );
 };
