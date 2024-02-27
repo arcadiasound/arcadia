d30402e7245dfae4f2c07a58b0ea00c466ac84db
gregogun <gregogun97@gmail.com>
2024-02-27 12:07:53 +0000

    feat: add profile sections

---
+++ apps/arcadia/src/modules/profile/components/Collection.tsx
@@ -0,0 +1,35 @@
+import { useGetUserProfile } from "@/hooks/appData";
+import { getTracks } from "@/lib/track/getTracks";
+import { TrackCard } from "@/modules/track/TrackCard";
+import { abbreviateAddress } from "@/utils";
+import { Box, Grid, Text } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+
+interface CollectionProps {
+  address: string;
+}
+
+export const Collection = (props: CollectionProps) => {
+  // update to query users collection
+  const { data: tracks } = useQuery({
+    queryKey: [`tracks`],
+    refetchOnWindowFocus: false,
+    queryFn: () => getTracks({ txids: undefined }),
+  });
+
+  const { data } = useGetUserProfile({ address: props.address });
+
+  const profile = data?.profiles.length ? data.profiles[0] : undefined;
+
+  return (
+    <Box mt="3">
+      <Grid asChild columns="6" gapX="2" gapY="7" width="auto">
+        <ul>
+          <Text weight="medium">
+            {profile?.name || abbreviateAddress({ address: props.address })}'s Collection
+          </Text>
+        </ul>
+      </Grid>
+    </Box>
+  );
+};
