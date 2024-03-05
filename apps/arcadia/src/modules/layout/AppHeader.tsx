f69b804ec2b902c6a690b6ac09ee09e5e9cfe903
gregogun <gregogun97@gmail.com>
2024-03-05 22:02:55 +0000

    chore: cleanup

--- apps/arcadia/src/modules/layout/AppHeader.tsx
+++ apps/arcadia/src/modules/layout/AppHeader.tsx
@@ -32,10 +32,6 @@ export const AppHeader = () => {
   const location = useLocation();
   const address = useActiveAddress();
 
-  useEffect(() => {
-    console.log(location);
-  }, [location]);
-
   return (
     <Flex
       style={css({
